3a4935211cf8121157effea804d8cefacef4b661
denuApp <89608039+denuApp@users.noreply.github.com>
2024-08-04 20:45:23 -0300

    set up de la base de datos

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import sequelize from "./config/database";
+import router from "./routes/routes";
+
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+
+app.use("/api", router);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, async () => {
+  console.log(`Server running on port ${PORT}`);
+  try {
+    sequelize.authenticate();
+    console.log("Database connected!");
+  } catch (error) {
+    console.log("Failed to connect to the database: ", error);
+  }
+});

58ee4a3882efa9b6c4f5b60e17eecd775127d0e3
denuApp <89608039+denuApp@users.noreply.github.com>
2024-08-04 21:57:45 -0300

    migraciones

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,36 @@
-require("dotenv").config();
+// require("dotenv").config();
+
+// module.exports = {
+//   development: {
+//     database: process.env.DB_NAME,
+//     username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     host: process.env.DB_HOST,
+//     dialect: "postgres",
+//     logging: false,
+//   },
+// };
 
 module.exports = {
   development: {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    dialect: "postgres",
-    logging: false,
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postegres',
+    database: process.env.DB_NAME || 'faculty_hub_db',
+    host: process.env.DATABASE_HOST || 'localhost',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'postgres',
+    password: null,
+    database: 'faculty_hub_db',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASSWORD || 'postegres',
+    database: process.env.DB_NAME || 'faculty_hub_db',
+    host: process.env.DATABASE_HOST || 'localhost',
+    dialect: 'postgres',
   },
 };

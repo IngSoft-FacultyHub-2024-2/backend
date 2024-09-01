b8df72c9d68a4159fb0fc457a47965b596a7ce77
denuApp <89608039+denuApp@users.noreply.github.com>
2024-09-01 19:39:51 -0300

    Update teacherRepository.ts

--- src/modules/teacher/repositories/teacherRepository.ts
+++ src/modules/teacher/repositories/teacherRepository.ts
@@ -14,9 +14,8 @@ class TeacherRepository {
     async addTeacher(teacher: Partial<Teacher>) {
         const transaction = await sequelize.transaction();
         try {
-            // Extraer y eliminar subjects_of_interest del objeto teacher
-            const subjectsOfInterestIds = teacher.subjects_of_interest || [];
-            delete teacher.subjects_of_interest;
+            // Extraer subjects_of_interest y otros datos del teacherData
+            const { subjects_of_interest = [], categories = [], benefits = [] } = teacher;
 
             // Crear el Teacher y sus asociaciones directas
             const newTeacher = await Teacher.create(teacher, {
@@ -30,32 +29,18 @@ class TeacherRepository {
             });
 
             // Asociar las categorías
-            if (teacher.categories && teacher.categories.length > 0) {
-                const categoryAssociations = teacher.categories.map(categoryId => ({
-                    teacher_id: newTeacher.id,
-                    category_id: categoryId,
-                }));
-                await TeacherCategory.bulkCreate(categoryAssociations, { transaction });
+            if (categories.length > 0) {
+                await this.associateCategories(newTeacher.id, categories, transaction);
             }
 
             // Asociar los beneficios
-            if (teacher.benefits && teacher.benefits.length > 0) {
-                const benefitAssociations = teacher.benefits.map(benefitId => ({
-                    teacher_id: newTeacher.id,
-                    benefit_id: benefitId,
-                }));
-                await TeacherBenefit.bulkCreate(benefitAssociations, { transaction });
+            if (benefits.length > 0) {
+                await this.associateBenefits(newTeacher.id, benefits, transaction);
             }
 
-
             // Asociar subjects_of_interest
-            if (subjectsOfInterestIds.length > 0) {
-                const subjectsOfInterest = subjectsOfInterestIds.map(id => ({
-                    subject_id: id,
-                    teacher_id: newTeacher.id,
-                }));
-
-                await TeacherSubjectOfInterest.bulkCreate(subjectsOfInterest, { transaction });
+            if (subjects_of_interest.length > 0) {
+                await this.associateSubjectsOfInterest(newTeacher.id, subjects_of_interest, transaction);
             }
 
             // Confirmar la transacción
@@ -63,12 +48,36 @@ class TeacherRepository {
 
             return newTeacher;
         } catch (error) {
-            console.error(error);
+            console.error('Error adding teacher:', error);
 
             // Realizar un rollback en caso de error
             await transaction.rollback();
             throw error;
         }
+    }
+
+    private async associateCategories(teacherId: number, categoryIds: any[], transaction: any) {
+        const categoryAssociations = categoryIds.map(categoryId => ({
+            teacher_id: teacherId,
+            category_id: categoryId,
+        }));
+        await TeacherCategory.bulkCreate(categoryAssociations, { transaction });
+    }
+
+    private async associateBenefits(teacherId: number, benefitIds: any[], transaction: any) {
+        const benefitAssociations = benefitIds.map(benefitId => ({
+            teacher_id: teacherId,
+            benefit_id: benefitId,
+        }));
+        await TeacherBenefit.bulkCreate(benefitAssociations, { transaction });
+    }
+
+    private async associateSubjectsOfInterest(teacherId: number, subjectIds: any[], transaction: any) {
+        const subjectAssociations = subjectIds.map(subjectId => ({
+            subject_id: subjectId,
+            teacher_id: teacherId,
+        }));
+        await TeacherSubjectOfInterest.bulkCreate(subjectAssociations, { transaction });
     }
 }
 

b7d40c0e86c9fcbfc1d6071ea8502a415e05b8b0
Andres Wilchinski <andreswil97@gmail.com>
2024-10-29 19:55:19 -0300

    fix: password and username on the app

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,13 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'ts-jest', 
-  testEnvironment: 'node', 
-  verbose: true, 
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
   testMatch: ['**/tests/**/*.test.ts'], 
   moduleFileExtensions: ['js', 'ts'], 
+  collectCoverage: true,
+  coverageDirectory: 'coverage', 
 };
 
 export default config;
--- jest.integration.config.ts
+++ jest.integration.config.ts
@@ -6,8 +6,6 @@ const config: Config = {
   roots: ['./tests/integration'], // Specify the root directory for integration tests
   testMatch: ['**/*.test.ts'], // Match test files in the specified directory
   moduleFileExtensions: ['js', 'ts'], // Recognizes JavaScript and TypeScript files
-  collectCoverage: true, // Collects test coverage information
-  coverageDirectory: 'coverage/integration', // Output directory for coverage reports
 };
 
 export default config;

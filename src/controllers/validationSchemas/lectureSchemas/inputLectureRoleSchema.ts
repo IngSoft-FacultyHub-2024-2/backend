5f8d5d3ad7f8404ec316791b43fad1c4eb923a5e
Andres Wilchinski <andreswil97@gmail.com>
2024-11-18 22:59:17 -0300

    add: number of teachers

--- src/controllers/validationSchemas/lectureSchemas/inputLectureRoleSchema.ts
+++ src/controllers/validationSchemas/lectureSchemas/inputLectureRoleSchema.ts
@@ -7,6 +7,7 @@ const inputLectureRoleSchema = yup.object().shape({
     .oneOf(getSubjectRoles(), "El rol debe ser Teórico o Tecnología")
     .required("El rol es requerido"),
     hour_configs: yup.array().of(inputLectureConfigHourSchema).required("Al menos una configuración de horas es requerida"),
+    number_of_teachers: yup.number().required("El número de profesores es requerido")
 });
 
 export default inputLectureRoleSchema;

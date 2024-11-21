d2ff86e9c8605de4f0f156a324fac0a4dd16ab9a
Andres Wilchinski <andreswil97@gmail.com>
2024-11-21 20:34:09 -0300

    feat: validate group

--- src/modules/semester/repositories/models/LectureGroup.ts
+++ src/modules/semester/repositories/models/LectureGroup.ts
@@ -3,47 +3,59 @@ import sequelize from '../../../../config/database';
 import Lecture from './Lecture';
 
 class LectureGroup extends Model {
-    public id!: number;
-    public lecture_id!: number;
-    public degree_id!: number;
-    public group!: string;
+  public id!: number;
+  public lecture_id!: number;
+  public degree_id!: number;
+  public group!: string;
 
-    public static associations: {
-        lecture: BelongsTo<LectureGroup, Lecture>;
-    };
+  public static associations: {
+    lecture: BelongsTo<LectureGroup, Lecture>;
+  };
 }
 
-LectureGroup.init({
+LectureGroup.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
     lecture_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        references: {
-            model: 'Lectures',
-            key: 'id',
-        },
-        onUpdate: 'CASCADE',
-        onDelete: 'CASCADE',
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Lectures',
+        key: 'id',
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE',
     },
     degree_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     group: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isValidGroup(value: string) {
+          const regex = /^(M|N|V)(10|[1-9])[A-Z]$/;
+          if (!regex.test(value)) {
+            throw new Error(
+              'Invalid group format. Must start with M, N, or V, followed by 1-10, and end with an uppercase letter.'
+            );
+          }
+        },
+      },
     },
-},
-    {
-        sequelize,
-        modelName: 'LectureGroup',
-        tableName: 'LectureGroups',
-        timestamps: true,
-      });
+  },
+  {
+    sequelize,
+    modelName: 'LectureGroup',
+    tableName: 'LectureGroups',
+    timestamps: true,
+  }
+);
 
 export default LectureGroup;

68b212627a23b3098dbe0cd0239567fd177fda5f
denuApp <89608039+denuApp@users.noreply.github.com>
2025-01-23 19:11:20 -0300

    semester study_plan_id allows null

--- src/modules/semester/repositories/models/Semester.ts
+++ src/modules/semester/repositories/models/Semester.ts
@@ -49,7 +49,7 @@ Semester.init(
     },
     study_plan_id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
       onUpdate: 'CASCADE',
       onDelete: 'CASCADE',
     },

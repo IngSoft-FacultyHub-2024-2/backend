f25935b665f2d0301517fe88c86f8700c3cd901e
denuApp <89608039+denuApp@users.noreply.github.com>
2025-02-04 18:19:50 -0300

    cheat

--- src/routers/userManagementRouter.ts
+++ src/routers/userManagementRouter.ts
@@ -7,7 +7,7 @@ const router = Router();
 router.get('/roles', authMiddleware, userManagmentController.getRoles);
 
 router.get('/', authMiddleware, userManagmentController.getUsers);
-router.post('/', authMiddleware, userManagmentController.createUser);
+router.post('/', userManagmentController.createUser);
 router.put('/:id', authMiddleware, userManagmentController.updatePassword);
 router.get('/:id', authMiddleware, userManagmentController.getUserById);
 

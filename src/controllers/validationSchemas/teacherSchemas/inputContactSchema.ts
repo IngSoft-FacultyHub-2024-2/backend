4908389678a5f53c6cdd1959b6d1096376e3410d
denuApp <89608039+denuApp@users.noreply.github.com>
2025-02-18 01:23:06 -0300

    fixes

--- src/controllers/validationSchemas/teacherSchemas/inputContactSchema.ts
+++ src/controllers/validationSchemas/teacherSchemas/inputContactSchema.ts
@@ -2,12 +2,30 @@ import * as yup from 'yup';
 
 const inputContactSchema = yup.object().shape({
   mean: yup.string().required('El medio de contacto es requerido'),
-  data: yup.string().when('mean', {
-    is: (mean: string) => mean.toLowerCase() === 'mail',
-    then: (schema) => schema.email('El correo debe ser válido'),
-    otherwise: (schema) =>
-      schema.required('La información de contacto es requerida'),
-  }),
+  data: yup
+    .string()
+    .when('mean', {
+      is: (mean: string) => mean.toLowerCase() === 'mail',
+      then: (schema) => schema.email('El correo debe ser válido'),
+      otherwise: (schema) =>
+        schema.required('La información de contacto es requerida'),
+    })
+    .when('mean', {
+      is: (mean: string) => mean.toLowerCase() === 'teams',
+      then: (schema) => schema.email('El correo de teams debe ser válido'),
+      otherwise: (schema) =>
+        schema.required('La información de contacto es requerida'),
+    })
+    .when('mean', {
+      is: (mean: string) => mean.toLowerCase() === 'whatsapp',
+      then: (schema) =>
+        schema.matches(
+          /^[0-9]{10}$/,
+          'El número de teléfono debe tener 10 dígitos'
+        ),
+      otherwise: (schema) =>
+        schema.required('La información de contacto es requerida'),
+    }),
   prefered: yup.boolean().required('Preference is required'),
 });
 export default inputContactSchema;
--- src/modules/teacher/repositories/models/Contact.ts
+++ src/modules/teacher/repositories/models/Contact.ts
@@ -1,55 +1,58 @@
-import { Model, DataTypes, BelongsTo } from 'sequelize';
+import { BelongsTo, DataTypes, Model } from 'sequelize';
 import sequelize from '../../../../config/database';
 import Teacher from './Teacher';
 
 class Contact extends Model {
-    public id!: number;
-    public teacher_id!: number;
-    public mean!: string;
-    public data!: string;
-    public prefered!: boolean;
+  public id!: number;
+  public teacher_id!: number;
+  public mean!: string;
+  public data!: string;
+  public prefered!: boolean;
 
-		public static associations: {
-			teacher: BelongsTo<Contact, Teacher>;
-		};
+  public static associations: {
+    teacher: BelongsTo<Contact, Teacher>;
+  };
 }
 
-Contact.init({
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true,
-  },
-  teacher_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-			model: 'Teachers',
-			key: 'id',
+Contact.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-	},
-  mean: {
-    type: DataTypes.STRING,
-    allowNull: false,
+    teacher_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Teachers',
+        key: 'id',
+      },
+    },
+    mean: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    data: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: {
+        name: 'data',
+        msg: 'La información de contacto ya existe',
+      },
+    },
+    prefered: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
   },
-  data: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: {
-      name: 'data',
-      msg: 'Contact data already exists',
-    }
-  },
-  prefered: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: false,
-  },
-}, {
-  sequelize,
-  modelName: 'Contact',
-  tableName: 'Contacts',
-  timestamps: true,
-});
+  {
+    sequelize,
+    modelName: 'Contact',
+    tableName: 'Contacts',
+    timestamps: true,
+  }
+);
 
 export default Contact;

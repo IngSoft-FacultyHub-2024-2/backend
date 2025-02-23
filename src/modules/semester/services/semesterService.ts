7b0dcee606f62f04eb30468c557c5ab02ea4a76a
andres <andreswil97@gamil.com>
2025-02-23 18:03:45 -0300

    fix: add the check of amiount of teachers assign to a lecture on the addLecture, it was only on updateLecture

--- src/modules/semester/services/semesterService.ts
+++ src/modules/semester/services/semesterService.ts
@@ -150,6 +150,15 @@ export async function addLecture(
   lecture: Partial<Lecture>,
   applyValidation = true
 ) {
+  const subject = await getSubjectById(lecture.subject_id!);
+  if (!subject) {
+    throw new ResourceNotFound(
+      'No se encontró la materia con id ' + lecture.subject_id
+    );
+  }
+  lecture.lecture_roles?.forEach((role) => {
+    checkAmountOfTeachersPerRole(role, subject);
+  });
   return await semesterRepository.addLecture(lecture, applyValidation);
 }
 
@@ -190,49 +199,7 @@ export async function updateLecture(
     ? (
         await Promise.all(
           lecture.lecture_roles.map(async (role) => {
-            if (subject.is_teo_tec_at_same_time) {
-              const expectedTeoTeachers =
-                subject.hour_configs
-                  ?.filter((config) => config.role === SubjectRoles.THEORY)
-                  .reduce((acc, config) => acc + 1, 0) || 0;
-              const expectedTecTeachers =
-                subject.hour_configs
-                  ?.filter((config) => config.role === SubjectRoles.TECHNOLOGY)
-                  .reduce((acc, config) => acc + 1, 0) || 0;
-
-              const teoTeachers = role.teachers.filter(
-                (teacher) => !teacher.is_technology_teacher
-              ).length;
-              const tecTeachers = role.teachers.filter(
-                (teacher) => teacher.is_technology_teacher
-              ).length;
-
-              if (teoTeachers > expectedTeoTeachers) {
-                throw new Error(
-                  `La cantidad de profesores asignados al rol 'Teórico' supera la cantidad permitida`
-                );
-              }
-
-              if (tecTeachers > expectedTecTeachers) {
-                throw new Error(
-                  `La cantidad de profesores asignados al rol
-                  'Tecnología' supera la cantidad permitida`
-                );
-              }
-            } else {
-              const subjectTeacherCount =
-                subject.hour_configs
-                  ?.filter((config) => config.role === role.role)
-                  .reduce((acc, config) => acc + 1, 0) || 0;
-
-              let teacherCount = role.teachers.length;
-
-              if (!subjectTeacherCount || teacherCount > subjectTeacherCount) {
-                throw new Error(
-                  `La cantidad de profesores asignados al rol '${role.role}' supera la cantidad permitida`
-                );
-              }
-            }
+            checkAmountOfTeachersPerRole(role, subject);
 
             const teachersPromises = role.teachers.map(async (teacher) => {
               const teacherData = await getTeacherById(
@@ -689,4 +656,53 @@ export async function submitTeacherReview(
     teacherId,
     reviewData
   );
+}
+
+function checkAmountOfTeachersPerRole(
+  role: LectureRole,
+  subject: SubjectResponseDto
+) {
+  if (subject.is_teo_tec_at_same_time) {
+    const expectedTeoTeachers =
+      subject.hour_configs
+        ?.filter((config) => config.role === SubjectRoles.THEORY)
+        .reduce((acc, config) => acc + 1, 0) || 0;
+    const expectedTecTeachers =
+      subject.hour_configs
+        ?.filter((config) => config.role === SubjectRoles.TECHNOLOGY)
+        .reduce((acc, config) => acc + 1, 0) || 0;
+
+    const teoTeachers = role.teachers.filter(
+      (teacher) => !teacher.is_technology_teacher
+    ).length;
+    const tecTeachers = role.teachers.filter(
+      (teacher) => teacher.is_technology_teacher
+    ).length;
+
+    if (teoTeachers > expectedTeoTeachers) {
+      throw new Error(
+        `La cantidad de profesores asignados al rol 'Teórico' supera la cantidad permitida`
+      );
+    }
+
+    if (tecTeachers > expectedTecTeachers) {
+      throw new Error(
+        `La cantidad de profesores asignados al rol
+        'Tecnología' supera la cantidad permitida`
+      );
+    }
+  } else {
+    const subjectTeacherCount =
+      subject.hour_configs
+        ?.filter((config) => config.role === role.role)
+        .reduce((acc, config) => acc + 1, 0) || 0;
+
+    let teacherCount = role.teachers.length;
+
+    if (!subjectTeacherCount || teacherCount > subjectTeacherCount) {
+      throw new Error(
+        `La cantidad de profesores asignados al rol '${role.role}' supera la cantidad permitida`
+      );
+    }
+  }
 }

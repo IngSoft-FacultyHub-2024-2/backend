1aaca37e508b5de2db9fe28ebca5d7aebd65c9bf
denuApp <89608039+denuApp@users.noreply.github.com>
2025-03-30 15:44:27 -0300

    Update userManagementRouter.ts

--- src/routers/userManagementRouter.ts
+++ src/routers/userManagementRouter.ts
@@ -1,15 +1,26 @@
 import { Router } from 'express';
 import userManagmentController from '../controllers/userManagmentController';
 import { authMiddleware } from '../middlewares/authMiddleware';
+import { isCoordinatorMiddleware } from '../middlewares/isCoordinatorMiddleware';
 
 const router = Router();
 
 router.get('/roles', authMiddleware, userManagmentController.getRoles);
 
 router.get('/', authMiddleware, userManagmentController.getUsers);
-router.post('/', authMiddleware, userManagmentController.createUser);
+router.post(
+  '/',
+  authMiddleware,
+  isCoordinatorMiddleware,
+  userManagmentController.createUser
+);
 router.put('/update-password', userManagmentController.updatePassword);
 router.get('/:id', authMiddleware, userManagmentController.getUserById);
-router.put('/:id', authMiddleware, userManagmentController.updateUser);
+router.put(
+  '/:id',
+  authMiddleware,
+  isCoordinatorMiddleware,
+  userManagmentController.updateUser
+);
 
 export default router;
